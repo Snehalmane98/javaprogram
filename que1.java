4d4df139fda8901367fccf8d4e1ce5d66a5a8ffa
Snehalmane98 <85489959+Snehalmane98@users.noreply.github.com>
2021-06-07 16:04:12 +0530

    done

---
+++ que1.java
@@ -0,0 +1,13 @@
+//1:write program to test Hello World.
+public class que1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("hello word");
+    for(int i=0;i<args.length;i++)
+    {
+    	System.out.println(args[i]);
+    }
+	}
+
+}
